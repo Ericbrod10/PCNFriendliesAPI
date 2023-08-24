0fc79bdbc0916cc4edcbbc17ae27d76ea75ef47e
Eric <56181235+Ericbrod10@users.noreply.github.com>
2023-08-24 01:36:50 -0400

    Added InQueue and showmatched states and DevID

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
+import Fingerprint2 from 'fingerprintjs2';
+import { SHA256 } from 'crypto-js';
 
 function CreateMyModelForm() {
   const [formData, setFormData] = useState({
@@ -14,7 +16,8 @@ function CreateMyModelForm() {
   });
 
   const [showForm, setShowForm] = useState(true);
-  const [showTimer, setShowTimer] = useState(false);
+  const [InQueue, setInQueue] = useState(false);
+  const [showmatched, setmatched] = useState(false);
   const [uniqueIdentifier, setUniqueIdentifier] = useState('');
   const [openCount, setOpenCount] = useState(0);
   const [player_name, setPlayerName] = useState('');
@@ -24,6 +27,10 @@ function CreateMyModelForm() {
   const [match_pref, setMatchPref] = useState('');
   const [player_pref, setPlayerPref] = useState('');
   const [open_or_close, setOpenOrClosed] = useState('');
+  const [opponent_manager, setopponent_manager] = useState('');
+  const [opponent_team, setopponent_team ] = useState('');
+  const [send_v_receive, setopponent_send_v_receive] = useState('');
+  const [elapsedTime, setElapsedTime] = useState(0);
 
 
   const handleChange = (event) => {
@@ -39,9 +46,9 @@ function CreateMyModelForm() {
       const response = await axios.post('http://localhost:8000/api/myendpoint/', formData);
       console.log(response.data);
       console.log(response.data.success);
-      if (response.data.success == true) {
+      if (response.data.success === true) {
       setShowForm(false); // Hide the form
-      setShowTimer(true); // Show the timer
+      setInQueue(true); // Show the timer
       setUniqueIdentifier(response.data.Unique_Identifier);
       console.log(response.data.Unique_Identifier); // Set the unique identifier
     } else console.log(response)} catch (error) {
@@ -50,7 +57,27 @@ function CreateMyModelForm() {
   };
 
   useEffect(() => {
-    if (showTimer) {
+    if (showForm) {
+      Fingerprint2.get(function(result, components) {
+        const hashedFingerprint = SHA256(result).toString();
+        formData.device_info = {
+          fingerprint: hashedFingerprint,
+          components: components
+        };
+        formData.device_info = formData.device_info.fingerprint.toString();
+      });
+
+      axios.get('https://api.ipify.org?format=json')
+      .then(response => {
+        formData.ip = response.data.ip;
+      })
+      .catch(error => {
+        console.error(error);
+      });
+      
+    }
+  
+    if (InQueue) {
       const fetchData = async () => {
         try {
           const response1 = await axios.get(`http://localhost:8000/api/myendpoint/${uniqueIdentifier}/`);
@@ -63,7 +90,17 @@ function CreateMyModelForm() {
           setMatchPref(response1.data.match_pref);
           setPlayerPref(response1.data.player_pref);
           setOpenOrClosed(response1.data.open_or_close);
-  
+          // if response1.data.Opponent_Unique_Identifier is not null, then state = 'MatchedScreen'
+          
+          if (response1.data.opponent_team != null) {
+            setInQueue(false);
+            setmatched(true);
+            setopponent_manager(response1.data.opponent_manager);
+            setopponent_team(response1.data.opponent_team);
+            setopponent_send_v_receive(response1.data.send_v_receive);
+
+          }
+
           // Do something with the response data
         } catch (error) {
           console.error(error);
@@ -82,10 +119,20 @@ function CreateMyModelForm() {
       const interval = setInterval(() => {
         fetchData();
       }, 15000);
+      // eslint-disable-next-line
+      let intervalId;
+      // eslint-disable-next-line
+      intervalId = setInterval(() => {
+        setElapsedTime(prevElapsedTime => prevElapsedTime + 1);
+      }, 1000);
   
       return () => clearInterval(interval);
     }
-  }, [showTimer, uniqueIdentifier]);
+  }, [InQueue, uniqueIdentifier]);
+  
+  const minutes = Math.floor(elapsedTime / 60);
+  const seconds = elapsedTime % 60
+
   return (
     <div>
       {showForm && (
@@ -120,22 +167,13 @@ function CreateMyModelForm() {
             <input type="text" name="player_pref" value={formData.player_pref} onChange={handleChange} />
           </label>
           <br />
-          <label>
-            IP:
-            <input type="text" name="ip" value={formData.ip} onChange={handleChange} />
-          </label>
-          <br />
-          <label>
-            Device Info:
-            <input type="text" name="device_info" value={formData.device_info} onChange={handleChange} />
-          </label>
-          <br />
-          
+
           <button type="submit">Submit</button>
         </form>
       )}
-      {showTimer && (
+      {InQueue && (
         <div>
+          <p>Elapsed Time: {minutes}:{seconds < 10 ? `0${seconds}` : seconds}</p>
           <p>Player Name: {player_name}</p>
           <p>Team Name: {team_name}</p>
           <p>Team League: {team_league}</p>
@@ -145,6 +183,22 @@ function CreateMyModelForm() {
           <p>Open or Closed: {open_or_close}</p>
           <p>Open Count: {openCount}</p>
         </div>
+      )}
+      {showmatched && (
+        <div>
+          <h3>Match Found!</h3>
+          {send_v_receive === 'Send' && (
+        <h3>Send a friendly invite to: {opponent_team}</h3>
+      )}
+      {send_v_receive === 'Receive' && (
+        <h3>Accept a friendly invite from: {opponent_team}</h3>
+      )}
+          <p>The other manager is: <b>{opponent_manager}</b></p>
+          <p>Reach out to the other manager if there are any issues accepting or sending.</p>
+
+          
+
+    </div>
       )}
     </div>
   );
